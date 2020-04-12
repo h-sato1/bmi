f4d185ce4cc441a54310ac27de129b5ec4cec76c
h-sato <h-sato@cries.co.jp>
2020-04-12 21:25:24 +0900

    Initial Commit

---
+++ MyBMIApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyBMIApp
+//
+//  Created by 佐藤秀樹 on 2020/04/12.
+//  Copyright © 2020年 佐藤秀樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
